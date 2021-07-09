f8b39a7794a31a60bd651dcc54a291357b66d471
Jhonatan Argoty <67934501+jhonaargoty@users.noreply.github.com>
2021-07-09 14:05:52 -0500

    x

--- pwa-react-test/src/App.js
+++ pwa-react-test/src/App.js
@@ -5,7 +5,7 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <header className="App-header">Test</header>
+      <header className="App-header">Testx</header>
     </div>
   );
 }
