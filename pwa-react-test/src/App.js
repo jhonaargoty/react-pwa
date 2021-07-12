105282b02052796415dacdf782b3437eeeb3e401
Jhonatan Argoty <67934501+jhonaargoty@users.noreply.github.com>
2021-07-12 10:21:56 -0500

    update changes

--- pwa-react-test/src/App.js
+++ pwa-react-test/src/App.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 
 import "./App.css";
 
 function App() {
+  const [luces, setLuces] = useState("OFF");
+
+  const test = () => {
+    if (luces === "OFF") {
+      setLuces("ON");
+    } else {
+      setLuces("OFF");
+    }
+  };
+
   return (
     <div className="App">
-      <header className="App-header">Testx</header>
+      <br />
+      <br />
+      <br />
+      <header className="App-header-x">Test PWA</header>
+      <br />
+      <br />
+      <button onClick={() => test()}>Luces</button>
+      <br />
+      <br />
+      {luces}
     </div>
   );
 }
